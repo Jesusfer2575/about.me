8d1a692519c7f3afb1148875d3b75e637f917d28
Ad Fernández <adfernandez@MacBook-Pro-de-Ad.local>
2020-10-18 09:33:07 -0500

    Unused components and imports deleted

--- src/pages/Setup/Setup.component.tsx
+++ src/pages/Setup/Setup.component.tsx
@@ -1,61 +1,16 @@
-import React, { FormEvent } from 'react';
+import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardMedia from '@material-ui/core/CardMedia';
-import SwipeableViews from 'react-swipeable-views';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs, { TabsActions } from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Box from '@material-ui/core/Box';
-import { Theme, useTheme } from '@material-ui/core/styles';
 
-import { TabPanelProps } from './Setup.types';
-import { useStyles, DividerHr } from './Setup.styles';
+import { useStyles } from './Setup.styles';
 import mySetupImg from '../../assets/img/setup.jpg';
 import { CardContent } from '@material-ui/core';
 
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`full-width-tabpanel-${index}`}
-      aria-labelledby={`full-width-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-function a11yProps(index: any) {
-  return {
-    id: `full-width-tab-${index}`,
-    'aria-controls': `full-width-tabpanel-${index}`,
-  };
-}
-
 const Setup: React.FC = () => {
   const classes = useStyles();
-  const theme = useTheme();
-  const [value, setValue] = React.useState(0);
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const handleChange = (event: any, newValue: number) => {
-    setValue(newValue);
-  };
-
-  const handleChangeIndex = (index: number) => {
-    setValue(index);
-  };
 
   return (
     <>
